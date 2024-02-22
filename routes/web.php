e46a2049a68fecb6d3101a7e6678ce36901784a7
hadipk97 <75596392+hadipk97@users.noreply.github.com>
2024-02-22 16:32:32 +0800

    make simpler coding for routes

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::group(['prefix' => 'idea/', 'as' => 'idea.'], function () {
-    Route::post('', [IdeaController::class, 'store'])->name('store');
+Route::resource('idea', IdeaController::class)->except(['index', 'create', 'show'])->middleware(['auth']);
 
-    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show');
+Route::resource('idea', IdeaController::class)->only(['show']);
 
-    Route::group(['middleware' => ['auth']], function () {
-        Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
-
-        Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
-
-        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
-
-        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
-    });
-});
+Route::resource('idea.comments', CommentController::class)->only(['store'])->middleware(['auth']);
 
 Route::get('/terms', function () {
     return view('terms');
