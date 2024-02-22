2b792c7ed46e1a13678da7f01b4b3c950fad3ee8
hadipk97 <75596392+hadipk97@users.noreply.github.com>
2024-02-22 16:09:04 +0800

    routing group

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
@@ -19,17 +20,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::get('/idea/{idea}', [IdeaController::class, 'show'])->name('idea.show');
+Route::group(['prefix' => 'idea/', 'as' => 'idea.'], function () {
+    Route::post('', [IdeaController::class, 'store'])->name('store');
 
-Route::get('/idea/{idea}/edit', [IdeaController::class, 'edit'])->name('idea.edit');
+    Route::get('/{idea}', [IdeaController::class, 'show'])->name('show');
 
-Route::put('/idea/{idea}', [IdeaController::class, 'update'])->name('idea.update');
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
 
-Route::post('/idea', [IdeaController::class, 'store'])->name('idea.store');
+        Route::put('/{idea}', [IdeaController::class, 'update'])->name('update');
 
-Route::delete('/idea/{idea}', [IdeaController::class, 'destroy'])->name('idea.destroy');
+        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
 
-Route::post('/idea/{idea}/comments', [CommentController::class, 'store'])->name('idea.comments.store');
+        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+    });
+});
 
 Route::get('/terms', function () {
     return view('terms');
