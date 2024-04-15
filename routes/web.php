aa7fdd8e4ac60d1cc1e4ba188233bcbf9f01d15d
hadipk97 <75596392+hadipk97@users.noreply.github.com>
2024-04-15 14:27:42 +0800

    update ideas

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\IdeaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +26,8 @@ Route::resource('idea', IdeaController::class)->except(['index', 'create', 'show
 Route::resource('idea', IdeaController::class)->only(['show']);
 
 Route::resource('idea.comments', CommentController::class)->only(['store'])->middleware(['auth']);
+
+Route::resource('users', UserController::class)->only('show', 'edit', 'update')->middleware('auth');
 
 Route::get('/terms', function () {
     return view('terms');
