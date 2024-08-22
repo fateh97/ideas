344af8919cef72b7438906f95c19b6fa04fc02d1
fateh97 <46086174+fateh97@users.noreply.github.com>
2024-08-22 17:09:02 +0800

    create active menu links

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,10 +1,21 @@
 @extends('layout.layout')
 
 @section('content')
-    <h1>Terms</h1>
-    <div>
-        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.)
-        that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless
-        filler text can be very useful when the focus is meant to be on design, not content.
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            <h1>Terms</h1>
+            <div>
+                The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.)
+                that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless
+                filler text can be very useful when the focus is meant to be on design, not content.
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
     </div>
 @endsection
